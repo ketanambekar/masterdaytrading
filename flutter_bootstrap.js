dc378d202be332f6e987df699971bbd79940eed0
Ketan Ambekar <krdambekar@gmail.com>
2025-07-10 19:20:25 +0530

    1.0.0+2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2275645826"
+    serviceWorkerVersion: "3336808063"
   }
 });
